1a6e8d199f1c9dbdf3459305a9e861bf3e3c6d29
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-04-25 07:59:56 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1972,7 +1972,8 @@
                 </div>
                 <div>
                     <label for="user_type" class="text-white p-2">Type of User:</label>
-                    <select id="user_type" name="user_type" required>
+                    <select id="user_type" name="user_type" required style="background-color: transparent;
+                    border: palevioletred 2px solid;">
                         <option value="artista">Artista</option>
                         <option value="colezionista">Collezionista</option>
                     </select>
