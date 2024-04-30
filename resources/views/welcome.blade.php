7a0bb816ebadba009620d03954cf7724a28bdba4
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-04-30 17:35:57 +0200

    rgrg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2012,23 +2012,23 @@
 
         </div>
         <!-- Modal -->
-{{--        <div class="modal fade @if(session()->has('success')) show d-block @endif--}}
-{{--" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">--}}
-{{--            <div class="modal-dialog">--}}
-{{--                <div class="modal-content">--}}
-{{--                    <div class="modal-header">--}}
-{{--                        <h5 class="modal-title" id="successModalLabel">Success</h5>--}}
-{{--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
-{{--                    </div>--}}
-{{--                    <div class="modal-body">--}}
-{{--                        {{ session('success') }}--}}
-{{--                    </div>--}}
-{{--                    <div class="modal-footer">--}}
-{{--                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        <div class="modal fade @if(session()->has('success')) show d-block @endif
+" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="successModalLabel">Success</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        {{ session('success') }}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
     </div>
