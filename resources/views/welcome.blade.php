d59d91d47be058eaf0a996d021753cfb7ae21019
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-05-01 17:08:33 +0200

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1320,7 +1320,7 @@
                                     <!-- <div style="width:100%; height:150px;position: relative;">
                                         <img src="img/dani.jpeg" style="width: 300px; height:200px;border-radius: 10px; position: absolute;" class="img-fluid">
                                     </div> -->
-                                    <div class="circular--portrait "><img src="img/dani.jpeg" ></div>
+                                    <div class="circular--portrait "><img src="img/dani1.jpg" ></div>
                                     <div class="title">
                                         <h6>CO-Founder</h6>
                                     </div>
@@ -1342,7 +1342,7 @@
                                     <!-- <div style="width: 100%; height:150px;position: relative;">
                                         <img src="img/siavashakabarzadeh-removebg-preview.png" style="width: 300px; height:200px;border-radius: 10px; position: absolute;">
                                     </div> -->
-                                    <div class="circular--portrait "><img src="img/siavashakabarzadeh-removebg-preview.png" ></div>
+                                    <div class="circular--portrait "><img src="img/siaaaa.jpg" ></div>
                                     <div class="title">
                                         <h6>CTO</h6>
                                     </div>
@@ -2014,10 +2014,11 @@
 
         </div>
         <!-- Modal -->
-        <div class="modal fade @if(session()->has('success')) show d-block @endif
-" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
+        <div class="modal  fade @if(session()->has('success')) show d-block @endif
+" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" data-toggle="modal" aria-hidden="true">
+            <!-- <div class="modal-dialog"> -->
+                <!-- <div class=" modalone">
+                <div class="modalDesign">
                     <div class="modal-header">
                         <h5 class="modal-title" id="successModalLabel">Success</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -2028,7 +2029,48 @@
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </div>
-                </div>
+                </div> -->
+                <div class="e-card playing">
+                    <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+  <div class="image"></div>
+  
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  
+
+      <div class="infotop">
+      <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+      <a href="#"><img src="img/logo12.svg" alt="" class="icon"></a>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="icon"> -->
+  <path fill="currentColor" d="M19.4133 4.89862L14.5863 2.17544C12.9911 1.27485 11.0089 1.27485 9.41368 2.17544L4.58674
+  4.89862C2.99153 5.7992 2 7.47596 2 9.2763V14.7235C2 16.5238 2.99153 18.2014 4.58674 19.1012L9.41368
+  21.8252C10.2079 22.2734 11.105 22.5 12.0046 22.5C12.6952 22.5 13.3874 22.3657 14.0349 22.0954C14.2204
+  22.018 14.4059 21.9273 14.5872 21.8252L19.4141 19.1012C19.9765 18.7831 20.4655 18.3728 20.8651
+  17.8825C21.597 16.9894 22 15.8671 22 14.7243V9.27713C22 7.47678 21.0085 5.7992 19.4133 4.89862ZM4.10784
+  14.7235V9.2763C4.10784 8.20928 4.6955 7.21559 5.64066 6.68166L10.4676 3.95848C10.9398 3.69152 11.4701
+  3.55804 11.9996 3.55804C12.5291 3.55804 13.0594 3.69152 13.5324 3.95848L18.3593 6.68166C19.3045 7.21476
+  19.8922 8.20928 19.8922 9.2763V9.75997C19.1426 9.60836 18.377 9.53091 17.6022 9.53091C14.7929 9.53091
+  12.1041 10.5501 10.0309 12.3999C8.36735 13.8847 7.21142 15.8012 6.68783 17.9081L5.63981 17.3165C4.69466
+  16.7834 4.10699 15.7897 4.10699 14.7235H4.10784ZM10.4676 20.0413L8.60933 18.9924C8.94996 17.0479 9.94402
+  15.2665 11.4515 13.921C13.1353 12.4181 15.3198 11.5908 17.6022 11.5908C18.3804 11.5908 19.1477 11.6864
+  19.8922 11.8742V14.7235C19.8922 15.2278 19.7589 15.7254 19.5119 16.1662C18.7615 15.3596 17.6806 14.8528
+   16.4783 14.8528C14.2136 14.8528 12.3781 16.6466 12.3781 18.8598C12.3781 19.3937 12.4861 19.9021 12.68
+   20.3676C11.9347 20.5316 11.1396 20.4203 10.4684 20.0413H10.4676Z"></path></svg><br>      
+  <p>Grazie per averci lasciato la tua e-mail!<br/>     
+  Hai appena fatto un tuffo nel fantastico mondo di Figarte!
+A breve riceverai una mail dal nostro team con tante belle notizie e sorprese artistiche.
+
+  </p>                         
+
+<div class="name"></div>
+  </div>
+</div>
             </div>
         </div>
 
@@ -2046,12 +2088,16 @@
             <div class="right_part">
                 <ul>
                     <li>
-                        <a class="creative_link" href="https://www.iubenda.com/privacy-policy/20286869">Privacy Policy</a>
-                        <a href="https://www.iubenda.com/privacy-policy/20286869" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy Policy</a>
+                    <a class="creative_link  iubenda-embed"  href="https://www.iubenda.com/privacy-policy/20286869">Privacy Policy</a>
+
+                        <!-- <a class="creative_link iubenda-white  iubenda-noiframe iubenda-embed iubenda-noiframe"  title="Privacy Policy " href=></a> -->
+                        <!-- <a href="https://www.iubenda.com/privacy-policy/20286869" class=" " title="Privacy Policy ">Privacy Policy</a> -->
                         <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
                     </li>
                     <li>
-                        <a class="creative_link" href="https://www.iubenda.com/privacy-policy/20286869/cookie-policy">Cookies</a>
+                    <a class="creative_link  iubenda-embed"  href="https://www.iubenda.com/privacy-policy/20286869/cookie-policy">Cookies</a>
+
+                        <!-- <a class="creative_link" href=""></a> -->
                         <script type="text/javascript">
                             (function (w,d) {
                                 var loader = function () {
@@ -2069,9 +2115,10 @@
                             })(window, document);
                         </script>
                     </li>
-                    <li>
-                        <a class="creative_link iubenda-white iubenda-noiframe iubenda-embed" href="https://www.iubenda.com/privacy-policy/20286869/cookie-policy" title="Cookie Policy">Cookie Policy</a>
-                        <script type="text/javascript">
+                    <!-- <li> -->
+                        <!-- <a class="creative_link " href="https://www.iubenda.com/privacy-policy/20286869/cookie-policy" title="Cookie Policy">Cookie Policy</a> -->
+                        <!-- iubenda-white iubenda-noiframe iubenda-embed -->
+                        <!-- <script type="text/javascript">
                             (function (w,d) {
                                 var loader = function () {
                                     var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0];
@@ -2086,10 +2133,11 @@
                                     w.onload = loader;
                                 }
                             })(window, document);
-                        </script>
-                    </li>
+                        </script> -->
+                    <!-- </li> -->
                     <li>
-                        <a class="creative_link iubenda-white iubenda-noiframe iubenda-embed" href="https://www.iubenda.com/termini-e-condizioni/20286869" title="Termini e Condizioni">Terms & Conditions</a>
+                        <a class="creative_link  iubenda-embed"  href="https://www.iubenda.com/termini-e-condizioni/20286869">Terms & Conditions</a>
+                        <!-- -->
                         <script type="text/javascript">
                             (function (w,d) {
                                 var loader = function () {
@@ -2179,10 +2227,11 @@
 <!-- !Main -->
 
 <!-- Scripts -->
-<script type="text/javascript" src="js/jquery.js?ver=3.1"></script>
-<script type="text/javascript" src="js/plugins.js?ver=3.1"></script>
-<!--[if lt IE 10]> <script type="text/javascript" src="js/ie8.js?ver=3.1"></script> <![endif]-->
-<script type="text/javascript" src="js/init.js?ver=3.1"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.js?ver=3.1') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins.js?ver=3.1') }}"></script>
+<script type="text/javascript" src="{{ asset('js/init.js?ver=3.1') }}"></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         var successMessage = "{{ session('success') }}";
