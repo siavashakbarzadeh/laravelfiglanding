7e701a65f4a37d713089da144d608fab4f7bf0c1
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-05-02 16:55:20 +0200

    button closee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2017,7 +2017,7 @@
         <!-- Modal -->
         <div class="modal  fade @if(session()->has('success')) show d-block @endif
 " id="successModal" tabindex="-1" aria-labelledby="successModalLabel" data-toggle="modal" aria-hidden="true">
-            <div class="e-card playing ">
+            <div class="e-card playing " id='myModal'>
                 <!-- modal-dialog  -->
                     <div class="image"></div>
 
@@ -2053,6 +2053,7 @@
                     <br>
                     Il team Figarte
                     </p>
+                    <span class="close-button">&times;</span>
 
 
                 </div>
@@ -2230,7 +2231,35 @@
             successModal.show();
         }
     });
+
+
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 </script>
+
 
 
 
