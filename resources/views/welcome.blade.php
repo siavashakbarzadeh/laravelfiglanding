68372d9168cf8e9ce4c485d38794b7f688c3bae9
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-05-02 16:58:50 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2053,7 +2053,7 @@
                     <br>
                     Il team Figarte
                     </p>
-                    <span class="close-button">&times;</span>
+                    <span class="close-button">close</span>
 
 
                 </div>
@@ -2061,6 +2061,33 @@
                    
             </div>
         </div>
+<script>
+        // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
 
 
     </div>
@@ -2233,31 +2260,7 @@
     });
 
 
-    // Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
-
-// Get the element that closes the modal
-var span = document.getElementsByClassName("close-button")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 </script>
 
 
