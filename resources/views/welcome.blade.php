0c60362c915f8a48c689bc30fbca49217d2c6480
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-05-02 15:17:10 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2029,14 +2029,14 @@
                     <div class="modal-header">
                     <a href="#"><img src="img/logo12.svg" alt="" class="icon"></a>
                         <!-- <h5 class="modal-title" id="successModalLabel">Success</h5> -->
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                     </div>
                     <div class="modal-body">
                         {{ session('success') }}
                     </div>
-                    <!-- <div class="modal-footer">
+                    <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div> -->
+                    </div>
                 </div>
                     <!-- <div class="e-card playing" >
 
